b697198ee6f984c7c9526a4955a65ba78a4960fd
poohc300 <poohc300@gmail.com>
2021-11-02 15:44:42 +0900

    입출금 기능 구현

--- trade/models.py
+++ trade/models.py
@@ -5,6 +5,7 @@ from django.db import models
 import os
 
 from django.db.models import query
+from requests.api import head
 import jwt
 import uuid
 import hashlib
@@ -12,27 +13,27 @@ import json
 from urllib.parse import urlencode
 
 import requests
+from dotenv import load_dotenv
 
 class Upbeat():
     def __init__(self):
-        self.access_key = 'wIGEVJC8Acp9BpypTKhlMZf8Z55Nabx9HTQBDaJj'
-        self.secret_key = 'JI8alCjBZi4RMObT2sixf2DC9PkpZ8lReeVz50lE'
-        self.server_url = 'https://api.upbit.com/v1/'
+        load_dotenv()
+        self.access_key = os.getenv('ACCESS_KEY')
+        self.secret_key = os.getenv('SECRET_KEY')
+        self.server_url = os.getenv('SERVER_URL')
 
-    def getQuery(self):
-        query = {
-            'market' :  'KRW-ADA'
-        }
+    def getQuery(self, query):
+        query = query
         return query
 
-    def getQueryString(self):
-        query_string = urlencode(self.getQuery()).encode()
+    def getQueryString(self, query):
+        query_string = urlencode(query).encode()
         
         return query_string
     
-    def getQueryHash(self):
+    def getQueryHash(self, query_string):
         m = hashlib.sha512()
-        m.update(self.getQueryString())
+        m.update(query_string)
         query_hash = m.hexdigest()
 
         return query_hash
@@ -47,41 +48,44 @@ class Upbeat():
 
         return payload
 
-    def getJwtToken(self):
-        jwt_token = jwt.encode(self.getPayload(), self.secret_key)
-
+    def getJwtToken(self, payload):
+        jwt_token = jwt.encode(payload, self.secret_key)
         return jwt_token
 
-    def getAuthorizeToken(self):
-        authorize_token = 'Bearer {}'.format(self.getJwtToken())
+    def getAuthorizeToken(self, jwt_token):
+        authorize_token = 'Bearer {}'.format(jwt_token)
 
         return authorize_token
 
-    def getHeaders(self):
+    def getHeaders(self, authorize_token):
         headers = {
-            "Authorization" : self.getAuthorizeToken()
+            "Authorization" : authorize_token
         }
         return headers
 
     def sendForm(
         self,
         route_name,
+        headers
         ):
         res = requests.get(
             self.server_url + route_name,
-            headers=self.getHeaders()
+            headers=headers
             )
         print(res.json())
         return res.json()
 
     def sendParamForm(
         self,
-        route_name
+        route_name,
+        query,
+        headers
         ):
+        print(headers)
         res = requests.get(
             self.server_url +  route_name,
-            params=self.getQuery(),
-            headers=self.getHeaders()
+            params=query,
+            headers=headers
             )
         print(res.json())
 
@@ -92,9 +96,19 @@ class Upbeat():
             전체 계좌 조회
         '''
         route_name = "accounts"
-        res = self.sendParamForm(
+        payload = {
+            'access_key' : self.access_key,
+            'nonce' : str(uuid.uuid4())
+        }
+        jwt_token = self.getJwtToken(payload)
+        authorize_token = self.getAuthorizeToken(jwt_token)
+        print(authorize_token)
+        headers = self.getHeaders(authorize_token)
+        res = self.sendForm(
             route_name=route_name,
+            headers=headers
             )
+        return res
 
     def getOrderChance(self):
         '''
@@ -102,11 +116,27 @@ class Upbeat():
         '''
         route_name = "orders/chance"
         query = {
-            'state' : 'wait'
+            'market' : 'KRW-ADA'
         }
+        query_string = self.getQueryString(query)
+        query_hash = self.getQueryHash(query_string)
+        payload = {
+            "access_key" : self.access_key,
+            'nonce' : str(uuid.uuid4()),
+            'query_hash' : query_hash,
+            'query_hash_alg' : "SHA512"
+        }
+        jwt_token = self.getJwtToken(payload)
+        authorize_token = self.getAuthorizeToken(jwt_token)
+        headers = self.getHeaders(authorize_token)
+
         res = self.sendParamForm(
             route_name=route_name,
+            query=query,
+            headers=headers
             )
+        return res
+
 
     def getOrderChanceById(self, id):
         '''
@@ -130,8 +160,24 @@ class Upbeat():
             주문 리스트 조회
         '''
         route_name = "orders"
+        query = {
+            'state': 'done',
+            }
+        query_string = self.getQueryString(query)
+        query_hash = self.getQueryHash(query_string)
+        payload = {
+            'access_key': self.access_key,
+            'nonce': str(uuid.uuid4()),
+            'query_hash': query_hash,
+            'query_hash_alg': 'SHA512',
+        }
+        jwt_token = self.getJwtToken(payload)
+        authorize_token = self.getAuthorizeToken(jwt_token)
+        headers = self.getHeaders(authorize_token)
         res = self.sendParamForm(
-            route_name=route_name
+            route_name=route_name,
+            query=query,
+            headers=headers
             )
 
     def getDepositList(self):
@@ -139,11 +185,28 @@ class Upbeat():
             입금 리스트 조회
         '''
         route_name = "deposits"
+        query = {
+            'currency': 'KRW',
+            }
+        query_string = self.getQueryString(query)
+        query_hash = self.getQueryHash(query_string)
+        payload = {
+            'access_key': self.access_key,
+            'nonce': str(uuid.uuid4()),
+            'query_hash': query_hash,
+            'query_hash_alg': 'SHA512',
+        }
+        jwt_token = self.getJwtToken(payload)
+        authorize_token = self.getAuthorizeToken(jwt_token)
+        headers = self.getHeaders(authorize_token)
+
         res = self.sendParamForm(
-            route_name=route_name
+            route_name=route_name,
+            query=query,
+            headers=headers
             )
 
-    def orderRequest(self, volume, price):
+    def orderRequest(self, volume, price, side_status):
         '''
             주문 하기
 
@@ -163,11 +226,12 @@ class Upbeat():
                     - market : 시장가 주문(매도)
                 identifier : 조회용 사용자 지정값
         '''
+        side = 'bid' if side_status == 1 else 'ask'
         route_name = "orders"
         ord_type = "limit"
         query = {
             'market' : 'KRW-ADA',
-            'side' : 'ask',
+            'side' : side,
             'volume' : volume,
             'price' : price,
             'ord_type' : ord_type
@@ -197,6 +261,9 @@ class Upbeat():
         print(res.json())
 
     def orderCancelRequest(self):
+        '''
+            주문 취소 요청
+        '''
         route_name = 'order'
         target_id = '3235d9cf-5e12-45da-8706-80aad6afc9f4'
         query = {
@@ -224,25 +291,313 @@ class Upbeat():
             )
         print(res.json())
 
+    def getWithrawsChance(self):
+        '''
+            출금 가능 정보
+        '''
+        route_name = 'withdraws/chance'
+        query = {
+            'currency' : 'ADA'
+        }
+        query_string = self.getQueryString(query)
+        query_hash = self.getQueryHash(query_string)
+        payload = {
+            'access_key': self.access_key,
+            'nonce': str(uuid.uuid4()),
+            'query_hash': query_hash,
+            'query_hash_alg': 'SHA512',
+        }
+        jwt_token = self.getJwtToken(payload)
+        authorize_token = self.getAuthorizeToken(jwt_token)
+        headers = self.getHeaders(authorize_token)
+        res = self.sendParamForm(
+            route_name=route_name,
+            query=query,
+            headers=headers
+            )
+
+    def withdrawCoin(self):
+        '''
+            코인 출금하기
+        '''
+        route_name = 'withdraws/coin'
+        query = {
+            'currency' : 'ADA',
+            'amount' : '0.01',
+            'address' : ''
+        }
+        query_string = self.getQueryString(query)
+        query_hash = self.getQueryHash(query_string)
+        payload = {
+            'access_key': self.access_key,
+            'nonce': str(uuid.uuid4()),
+            'query_hash': query_hash,
+            'query_hash_alg': 'SHA512',
+        }
+        jwt_token = self.getJwtToken(payload)
+        authorize_token = self.getAuthorizeToken(jwt_token)
+        headers = self.getHeaders(authorize_token)
+
+        res = self.sendParamForm(
+            route_name=route_name,
+            query=query,
+            headers=headers
+        )
+
+    def withdrawKrw(self):
+        '''
+            원화 출금하기
+        '''
+        route_name = 'withdraws/krw'
+        query = {
+           'amount' : '10000'
+        }
+        query_string = self.getQueryString(query)
+        query_hash = self.getQueryHash(query_string)
+        payload = {
+            'access_key': self.access_key,
+            'nonce': str(uuid.uuid4()),
+            'query_hash': query_hash,
+            'query_hash_alg': 'SHA512',
+        }
+        jwt_token = self.getJwtToken(payload)
+        authorize_token = self.getAuthorizeToken(jwt_token)
+        headers = self.getHeaders(authorize_token)
+
+        res = self.sendParamForm(
+            route_name=route_name,
+            query=query,
+            headers=headers
+        )
+
+    def getDeposit(self, uuid_value):
+        '''
+            개별 입금 조회
+        ''' 
+        route_name = 'deposit'
+        query = {
+            'uuid' : uuid_value
+        }
+        query_string = self.getQueryString(query)
+        query_hash = self.getQueryHash(query_string)
+        payload = {
+            'access_key': self.access_key,
+            'nonce': str(uuid.uuid4()),
+            'query_hash': query_hash,
+            'query_hash_alg': 'SHA512',
+        }
+        jwt_token = self.getJwtToken(payload)
+        authorize_token = self.getAuthorizeToken(jwt_token)
+        headers = self.getHeaders(authorize_token)
+
+        res = self.sendParamForm(
+            route_name=route_name,
+            query=query,
+            headers=headers
+        )
+
+    def generateCoinAddressForDeposit(self):
+        '''
+            입금 주소 생성 요청
+        '''
+        route_name = 'deposits/generate_coin_address'
+        query = {
+            'currency' : 'ADA'
+        }
+        query_string = self.getQueryString(query)
+        query_hash = self.getQueryHash(query_string)
+        payload = {
+            'access_key': self.access_key,
+            'nonce': str(uuid.uuid4()),
+            'query_hash': query_hash,
+            'query_hash_alg': 'SHA512',
+        }
+        jwt_token = self.getJwtToken(payload)
+        authorize_token = self.getAuthorizeToken(jwt_token)
+        headers = self.getHeaders(authorize_token)
+
+        res = requests.post(
+            self.server_url + route_name,
+            params=query,
+            headers=headers
+        )
+
+    def getAllCoinAddressForDeposit(self):
+        '''
+            전체 임금 주소 조회
+        '''
+        route_name = 'deposits/coin_address'
+        payload = {
+            'access_key' : self.access_key,
+            'nonce' : str(uuid.uuid4())
+        }
+        jwt_token = self.getJwtToken(payload)
+        authorize_token = self.getAuthorizeToken(jwt_token)
+        headers = self.getHeaders(authorize_token)
+        
+        res = self.sendForm(
+            route_name=route_name,
+            headers=headers
+        )
+
+    def getCoinAddressForDeposit(self):
+        '''
+            개별 입금 주소 조회
+        '''
+        route_name = 'deposits/coin_address'
+        query = {
+            'currency' : 'ADA'
+        }
+        query_string = self.getQueryString(query)
+        query_hash = self.getQueryHash(query_string)
+        payload = {
+            'access_key': self.access_key,
+            'nonce': str(uuid.uuid4()),
+            'query_hash': query_hash,
+            'query_hash_alg': 'SHA512',
+        }
+        jwt_token = self.getJwtToken(payload)
+        authorize_token = self.getAuthorizeToken(jwt_token)
+        headers = self.getHeaders(authorize_token)
+
+        res = self.sendParamForm(
+            route_name=route_name,
+            query=query,
+            headers=headers
+        )
+
+    def requestDepositKrw(self):
+        '''
+            원화 입금하기
+        '''
+        route_name = 'deposity/krw'
+        query = {
+            'amount' : '10000'
+        }
+        query_string = self.getQueryString(query)
+        query_hash = self.getQueryHash(query_string)
+        payload = {
+            'access_key': self.access_key,
+            'nonce': str(uuid.uuid4()),
+            'query_hash': query_hash,
+            'query_hash_alg': 'SHA512',
+        }
+        jwt_token = self.getJwtToken(payload)
+        authorize_token = self.getAuthorizeToken(jwt_token)
+        headers = self.getHeaders(authorize_token)
+
+        res = requests.post(
+            self.server_url + route_name,
+            params=query,
+            headers=headers
+        )
+
+    def getWalletStatus(self):
+        '''
+            입출금 현황
+        '''
+        route_name = 'status/wallet'
+        payload = {
+            'access_key' : self.access_key,
+            'nonce' : str(uuid.uuid4())
+        }
+        jwt_token = self.getJwtToken(payload)
+        authorize_token = self.getAuthorizeToken(jwt_token)
+        headers = self.getHeaders(authorize_token)
+        
+        res = self.sendForm(
+            route_name=route_name,
+            headers=headers
+        )
+
+    def getApiKeys(self):
+        '''
+            API 키 리스트 조회
+        '''
+        route_name = 'api_keys'
+        payload = {
+            'access_key' : self.access_key,
+            'nonce' : str(uuid.uuid4())
+        }
+        jwt_token = self.getJwtToken(payload)
+        authorize_token = self.getAuthorizeToken(jwt_token)
+        headers = self.getHeaders(authorize_token)
+        
+        res = self.sendForm(
+            route_name=route_name,
+            headers=headers
+        )
+'''
+    테스트 코드
+'''
 upbeat = Upbeat()
 # 전체 계좌 조회
 account = upbeat.getAllAccount()
-print(account)
+print(f'전체계좌조회: {account}')
 # 주문 가능 정보
 order = upbeat.getOrderChance()
-print(order)
-# 주문하기 (매도)
-'''
+print(f'주문가능정보: {order}')
+
+# 주문하기 
+# side_status 가 1이면 매수 
+# side_statsu 가 1이 아닌 경우는 매도
+
 order = upbeat.orderRequest(
     volume = '2',
-    price = '5000'
+    price = '5000',
+    side_status=1
 )
-'''
+
 # 주문 리스트 조회
 orderList = upbeat.getOrderList()
-print(orderList)
+print(f'주문리스트 조회: {orderList}')
+
 # 주문 취소
 orderCancel = upbeat.orderCancelRequest()
+print(f'주문취소: {orderCancel}')
 
+
+# 출금 가능 정보
+withdrawChance = upbeat.getWithrawsChance()
+print(f'출금가능 정보: {withdrawChance}')
+
+# 코인 출금하기
+withdrawCoin = upbeat.withdrawCoin()
+print(f'코인 출금하기: {withdrawCoin}')
+
+# 원화 출금하기
+withdrawKrw = upbeat.withdrawKrw()
+print(f'원화 출금하기: {withdrawKrw}')
+
+# 입금리스트 조회
 depositList = upbeat.getDepositList()
-print(depositList)
+print(f'입금리스트 조회: {depositList}')
+
+# 개별 입금 조회
+# 상세 검색이니 입금 uuid 가 파라메타에 필요함
+deposit = upbeat.getDeposit(uuid_value='')
+print(f'개별 입금 조회: {deposit}')
+
+# 입금 주소 생성 요청
+coinAddressForDeposit = upbeat.generateCoinAddressForDeposit()
+print(f'입금 주소 생성 요청: {coinAddressForDeposit}')
+
+# 전체 입금 주소 조회
+allCoinAddressForDeposit = upbeat.getAllCoinAddressForDeposit()
+print(f'전체 입금 주소 조회: {coinAddressForDeposit}')
+
+# 개별 입금 주소 조회
+coinAddressForDeposit = upbeat.getCoinAddressForDeposit()
+print(f'개별 입금 주소 조회: {coinAddressForDeposit}')
+
+# 원화 입금 하기
+depositKrw = upbeat.requestDepositKrw()
+print(f'원화 입금 하기: {depositKrw}')
+
+# 입출금 현황
+walletStatus = upbeat.getWalletStatus()
+print(f'입출금 현황: {walletStatus}')
+
+# API 키 리스트 조회
+apiKeys = upbeat.getApiKeys()
+print(f'API 키 리스트 조회: {apiKeys}')
