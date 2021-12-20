f9da561e881d46f0281701e0e1464b65728f7f20
Jeremy <poohc300@gmail.com>
2021-12-20 18:18:18 +0900

    test

--- trade/models.py
+++ trade/models.py
@@ -8,26 +8,6 @@ from django.db.models import query
 from urllib.parse import urlencode
 import datetime
 
-class User(models.Model):
-    name = models.CharField(max_length=60)
-    id = models.AutoField(primary_key=True)
-    email = models.CharField(max_length=200)
-    api_key = models.CharField(max_length=200)
-    secret_key = models.CharField(max_length=200)
-    created_at = models.DateField(auto_now_add=True)
-
-    class Meta:
-        ordering = ['created_at']
-
-class Accounts(models.Model):
-    user_id = models.ForeignKey('User', on_delete=models.CASCADE)
-    id = models.AutoField(primary_key=True)
-    balance = models.FloatField(max_length=200)
-    created_at = models.DateField(auto_now_add=True)
-    updated_at = models.DateField(default=datetime.datetime.now())
-
-    class Meta:
-        ordering = ['created_at']
 
 class Order(models.Model):
     id = models.AutoField(primary_key=True)
