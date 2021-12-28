be0f58e952aa9c5d544d52baabc57a92204404d1
Jeremy <poohc300@gmail.com>
2021-12-28 14:43:35 +0900

    trade 앱에 swagger 적용

--- trade/views.py
+++ trade/views.py
@@ -19,8 +19,6 @@ from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
 
 
-    
-
 @permission_classes([AllowAny])
 class OrderView(APIView):
 
@@ -33,7 +31,21 @@ class OrderView(APIView):
             market=market,
             days_number=days
         )
-
+    @swagger_auto_schema(
+        operation_summary="주문 하기",
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'ord_type' : openapi.Schema(type=openapi.TYPE_STRING, description='limit: 지정가 market: 시장가'),
+                'is_bid' : openapi.Schema(type=openapi.TYPE_STRING, description='1이면 매수 0이면 매도'),
+                'price' : openapi.Schema(type=openapi.TYPE_STRING, description='주문 가격'),
+                'volume' : openapi.Schema(type=openapi.TYPE_STRING, description='주문 사이즈'),
+                'market' : openapi.Schema(type=openapi.TYPE_STRING, description='ex) KRW-BTC'),
+            }
+        ),
+        tags=["trade"],
+        operation_description="order",
+    )  
     def post(self, request, *args, **kwargs) -> HttpResponse:
         '''
             ord_type 따라 다름
@@ -104,7 +116,17 @@ class OrderBookView(APIView):
             market=market,
             days_number=days
         )
-
+    @swagger_auto_schema(
+        operation_summary="오더북 조회 하기",
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'market' : openapi.Schema(type=openapi.TYPE_STRING, description='ex) KRW-BTC')
+            }
+        ),
+        tags=["trade"],
+        operation_description="orderbook list",
+    )  
     def post(self, request, *args, **kwargs) -> HttpResponse:
         data = request.data['body']
         
@@ -137,7 +159,19 @@ class AccountView(APIView):
             market=market,
             days_number=days
         )
-
+    @swagger_auto_schema(
+        operation_summary="현재 사용자 계좌정보 조회 하기",
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'access_key' : openapi.Schema(type=openapi.TYPE_STRING, description='access key'),
+                'secret_key' : openapi.Schema(type=openapi.TYPE_STRING, description='secret key'),
+                'market' : openapi.Schema(type=openapi.TYPE_STRING, description='ex) KRW-BTC')
+            }
+        ),
+        tags=["trade"],
+        operation_description="retrieve current user's balance",
+    )  
     def post(self, request, *args, **kwargs) -> HttpResponse:
         data = request.data['body']
         dto = self.build_dto(
@@ -205,6 +239,19 @@ class OrderListView(APIView):
             days_number=days
         )
 
+    @swagger_auto_schema(
+        operation_summary="완료 & 취소된 주문 내역 조회하기",
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'access_key' : openapi.Schema(type=openapi.TYPE_STRING, description='access key'),
+                'secret_key' : openapi.Schema(type=openapi.TYPE_STRING, description='secret key'),
+                'market' : openapi.Schema(type=openapi.TYPE_STRING, description='ex) KRW-BTC')
+            }
+        ),
+        tags=["trade"],
+        operation_description="retrieve done & canceled order list",
+    )  
     def post(self, request, *args, **kwargs) -> HttpResponse:
         data = request.data['body']
         dto = self.build_dto(
@@ -239,7 +286,19 @@ class UnfinishedOrderListView(APIView):
             market=market,
             days_number=days
         )
-
+    @swagger_auto_schema(
+        operation_summary="미체결 주문 내역 조회하기",
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'access_key' : openapi.Schema(type=openapi.TYPE_STRING, description='access key'),
+                'secret_key' : openapi.Schema(type=openapi.TYPE_STRING, description='secret key'),
+                'market' : openapi.Schema(type=openapi.TYPE_STRING, description='ex) KRW-BTC')
+            }
+        ),
+        tags=["trade"],
+        operation_description="retrieve unfinished order list",
+    )  
     def post(self, request, *args, **kwargs) -> HttpResponse:
         data = request.data['body']
         dto = self.build_dto(
@@ -274,7 +333,20 @@ class OrderCancelView(APIView):
             market=market,
             days_number=days
         )
-
+    @swagger_auto_schema(   
+        operation_summary="주문 취소하기",
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'access_key' : openapi.Schema(type=openapi.TYPE_STRING, description='access key'),
+                'secret_key' : openapi.Schema(type=openapi.TYPE_STRING, description='secret key'),
+                'market' : openapi.Schema(type=openapi.TYPE_STRING, description='ex) KRW-BTC'),
+                'uuid' : openapi.Schema(type=openapi.TYPE_STRING, description='ex) 주문 uuid')
+            }
+        ),
+        tags=["trade"],
+        operation_description="delete order by uuid",
+    )  
     def post(self, request, *args, **kwargs) -> HttpResponse:
         data = request.data['body']
         dto = self.build_dto(
@@ -295,36 +367,3 @@ class OrderCancelView(APIView):
 
         return HttpResponse(result)
 
-
-@permission_classes([AllowAny])
-class OrderCancelView(APIView):
-
-    def build_dto(self, access_key, secret_key,server_url, market, days) -> UpbitDTO:
-        #load_dotenv()
-        return UpbitDTO(
-            access_key=access_key,
-            secret_key=secret_key,
-            server_url=server_url,
-            market=market,
-            days_number=days
-        )
-
-    def post(self, request, *args, **kwargs) -> HttpResponse:
-        data = request.data['body']
-        dto = self.build_dto(
-            access_key = data['access_key'],
-            secret_key = data['secret_key'],
-            server_url="https://api.upbit.com/v1/",
-            market = data['market'],
-            days=2
-        )
-        service = Strategy(dto)
-        try:
-            result = service.orderCancelRequest(
-                id=data['order_uuid']
-            )
-            print(result)
-        except Exception as e:
-            return HttpResponse(e)
-
-        return HttpResponse(result)
