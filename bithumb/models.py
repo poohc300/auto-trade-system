5dddcbd7c16fe50799438dbd124217242512ebd4
Jeremy <poohc300@gmail.com>
2021-12-28 18:15:34 +0900

    get 방식 리팩토링

--- bithumb/models.py
+++ bithumb/models.py
@@ -10,144 +10,102 @@ import hmac
 import time
 import urllib
 import requests
-from requests.adapters import HTTPAdapter
-from urllib3.util.retry import Retry
 
 
 
 
-class PublicApi:
-    @staticmethod
-    def ticker(order_currency, payment_currency="KRW"):
-        uri = "/public/ticker/{}_{}".format(order_currency, payment_currency)
-        return BithumbHttp().get(uri)
+class BithumbAPI:
 
-    @staticmethod
-    def transaction_history(order_currency, payment_currency="KRW", limit=20):
-        uri = "/public/transaction_history/{}_{}?count={}".format(order_currency,
-                                                    payment_currency,
-                                                    limit)
-        return BithumbHttp().get(uri)
-
-    @staticmethod
-    def orderbook(order_currency, payment_currency="KRW", limit=5):
-        uri = "/public/orderbook/{}_{}?count={}".format(order_currency,
-                                                     payment_currency, limit)
-        return BithumbHttp().get(uri)
-
-    @staticmethod
-    def btci():
-        uri = "/public/btci"
-        return BithumbHttp().get(uri)
-
-    @staticmethod
-    def candlestick(order_currency, payment_currency="KRW", chart_intervals="24h"):
-        uri = "/public/candlestick/{}_{}/{}".format(order_currency, payment_currency,
-                                                chart_intervals)
-        return BithumbHttp().get(uri)
+    def __init__(self, api_key, secret_key):
+        self.api_key = api_key
+        self.secret_key = secret_key
+        self.base_url = 'https://api.bithumb.com'
 
 
-
-class PrivateApi:
-    def __init__(self, conkey, seckey):
-        self.http = BithumbHttp(conkey, seckey)
-
-    def account(self, **kwargs):
-        return self.http.post('/info/account', **kwargs)
-
-    def balance(self, **kwargs):
-        return self.http.post('/info/balance', **kwargs)
-
-    def place(self, **kwargs):
-        return self.http.post('/trade/place', **kwargs)
-
-    def orders(self, **kwargs):
-        return self.http.post('/info/orders', **kwargs)
-
-    def order_detail(self, **kwargs):
-        return self.http.post('/info/order_detail', **kwargs)
-
-    def cancel(self, **kwargs):
-        return self.http.post('/trade/cancel', **kwargs)
-
-    def market_buy(self, **kwargs):
-        return self.http.post('/trade/market_buy', **kwargs)
-
-    def market_sell(self, **kwargs):
-        return self.http.post('/trade/market_sell', **kwargs)
-
-    def withdraw_coin(self, **kwargs):
-        return self.http.post('/trade/btc_withdrawal', **kwargs)
-
-    def withdraw_cash(self, **kwargs):
-        return self.http.post('/trade/krw_withdrawal', **kwargs)
-
-
-class HttpMethod:
-    def __init__(self):
-        self.session = self._requests_retry_session()
-
-    def _requests_retry_session(retries=5, backoff_factor=0.3,
-                                status_forcelist=(500, 502, 504), session=None):
-        s = session or requests.Session()
-        retry = Retry(total=retries, read=retries, connect=retries,
-                      backoff_factor=backoff_factor,
-                      status_forcelist=status_forcelist)
-        adapter = HTTPAdapter(max_retries=retry)
-        s.mount('http://', adapter)
-        s.mount('https://', adapter)
-        return s
-
-    @property
-    def base_url(self):
-        return ""
-
-    def update_headers(self, headers):
-        self.session.headers.update(headers)
-
-    def post(self, path, timeout=3, **kwargs):
-        try:
-            uri = self.base_url + path
-            return self.session.post(url=uri, data=kwargs, timeout=timeout).\
-                json()
-        except Exception as x:
-            print("It failed", x.__class__.__name__)
-            return None
-
-    def get(self, path, timeout=3, **kwargs):
-        try:
-            uri = self.base_url + path
-            return self.session.get(url=uri, params=kwargs, timeout=timeout).\
-                json()
-        except Exception as x:
-            print("It failed", x.__class__.__name__)
-            return None
-
-
-class BithumbHttp(HttpMethod):
-    def __init__(self, conkey="", seckey=""):
-        self.API_CONKEY = conkey.encode('utf-8')
-        self.API_SECRET = seckey.encode('utf-8')
-        super(BithumbHttp, self).__init__()
-
-    @property
-    def base_url(self):
-        return "https://api.bithumb.com"
 
     def _signature(self, path, nonce, **kwargs):
         query_string = path + chr(0) + urllib.parse.urlencode(kwargs) + \
                        chr(0) + nonce
-        h = hmac.new(self.API_SECRET, query_string.encode('utf-8'),
+        h = hmac.new(self.secret_key, query_string.encode('utf-8'),
                      hashlib.sha512)
         return base64.b64encode(h.hexdigest().encode('utf-8'))
 
-    def post(self, path, **kwargs):
-        kwargs['endpoint'] = path
+    def build_header(self, path, **kwargs):
         nonce = str(int(time.time() * 1000))
 
-        self.update_headers({
-            'Api-Key': self.API_CONKEY,
+        headers={
+            'Api-Key': self.api_key,
             'Api-Sign': self._signature(path, nonce, **kwargs),
             'Api-Nonce': nonce
-        })
-        return super().post(path, **kwargs)
+        }
+        return headers
+
+    def build_get_form(self, path, timeout=3, **kwargs):
+
+        try:
+            uri = self.base_url + path
+            result = requests.get(uri)
+            print(result)
+
+            return result.json()
+
+        except Exception as e:
+            return e
+
+    def build_post_form(self, path, timeout=3, **kwargs):
+
+        try:
+            url = self.base_url + path
+            result = requests.post(
+                url=url,
+                headers=self.build_header(path=path),
+                data=kwargs['data']
+            )
+            return result.json()
+
+        except Exception as e:
+            return e
+
+    def ticker(self, order_currency, payment_currency="KRW"):
+        uri = "/public/ticker/{}_{}".format(order_currency, payment_currency)
+        return self.build_get_form(uri)
+
+    
+    def transaction_history(self, order_currency, payment_currency="KRW", limit=20):
+        uri = "/public/transaction_history/{}_{}?count={}".format(order_currency,
+                                                    payment_currency,
+                                                    limit)
+        return self.build_get_form(uri)
+
+    
+    def orderbook(self, order_currency, payment_currency, limit):
+        print(order_currency)
+        uri = f"/public/orderbook/{order_currency}_{payment_currency}?count={limit}"
+        print(uri)
+        return self.build_get_form(uri)
+
+
+    def account(self, **kwargs):
+        return self.build_post_form('/info/account', **kwargs)
+
+    def balance(self, **kwargs):
+        return self.build_post_form('/info/balance', **kwargs)
+
+    def place(self, **kwargs):
+        return self.build_post_form('/trade/place', **kwargs)
+
+    def orders(self, **kwargs):
+        return self.build_post_form('/info/orders', **kwargs)
+
+    def order_detail(self, **kwargs):
+        return self.build_post_form('/info/order_detail', **kwargs)
+
+    def cancel(self, **kwargs):
+        return self.build_post_form('/trade/cancel', **kwargs)
+
+    def market_buy(self, **kwargs):
+        return self.build_post_form('/trade/market_buy', **kwargs)
+
+    def market_sell(self, **kwargs):
+        return self.build_post_form('/trade/market_sell', **kwargs)
+
--- bithumb/views.py
+++ bithumb/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from .services import Bithumb
+from .services import BithumbService
 from rest_framework.views import APIView
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -13,13 +13,30 @@ from dotenv import load_dotenv
 from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
 from rest_framework.decorators import permission_classes, authentication_classes
 from abc import *
-
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 @permission_classes([AllowAny])
 class OrderView(APIView):
 
+    @swagger_auto_schema(   
+        operation_summary="주문 하기",
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'order_currency' : openapi.Schema(type=openapi.TYPE_STRING, description='구매할 코인 통화'),
+                'unit' : openapi.Schema(type=openapi.TYPE_STRING, description='주문 수량'),
+                'price' : openapi.Schema(type=openapi.TYPE_STRING, description='주문 가격'),
+                'api_key' : openapi.Schema(type=openapi.TYPE_STRING, description='api key'),
+                'api_secret' : openapi.Schema(type=openapi.TYPE_STRING, description='api secret'),
+                'status' : openapi.Schema(type=openapi.TYPE_STRING, description='0: 지정가 매수 1: 지정가 매도 2: 시장가 매수 3: 시장가 매도')
+            }
+        ),
+        tags=["bithumb"],
+        operation_description="order",
+    )  
     def post(self, request, *args, **kwargs) -> HttpResponse:
-        data = request.data['body']
+        data = request.data
         _order_currency = data['order_currency']
         _unit = data['unit']
         _price = data['price']
@@ -27,7 +44,7 @@ class OrderView(APIView):
         _api_secret = data['api_secret']
         _status = data['status']
 
-        bithumb = Bithumb(
+        bithumb = BithumbService(
             conkey=_api_key,
             seckey=_api_secret
         )
@@ -75,34 +92,61 @@ class OrderView(APIView):
 @permission_classes([AllowAny])
 class OrderBookView(APIView):
 
-    def get(self, request, *args, **kwargs) -> HttpResponse:
-        data = request.data['body']
+    @swagger_auto_schema(   
+        operation_summary="오더북 리스트 조회하기",
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'api_key' : openapi.Schema(type=openapi.TYPE_STRING, description='api key'),
+                'api_secret' : openapi.Schema(type=openapi.TYPE_STRING, description='api secret'),
+                'order_currency' : openapi.Schema(type=openapi.TYPE_STRING, description='통화 종류'),
+                'payment_currency' : openapi.Schema(type=openapi.TYPE_STRING, description='구매할 통화 KRW'),
+            }
+        ),
+        tags=["bithumb"],
+        operation_description="retrieve orderbook list",
+    )  
+    def post(self, request, *args, **kwargs) -> HttpResponse:
+        data = request.data
         _order_currency = data['order_currency']
         _payment_currency = data['payment_currency']
         _api_key = data['api_key']
         _api_secret = data['api_secret']    
-
-        bithumb = Bithumb(
+        bithumb = BithumbService(
             conkey=_api_key,
             seckey=_api_secret
         )
         try:
            
             result = bithumb.get_orderbook(
-                order_currency=_order_currency
+                order_currency=_order_currency,
+                payment_currency=_payment_currency
             )
-
+            print(result)
         except Exception as e:
             return HttpResponse(e)
 
-        return JsonResponse(result)
+        return HttpResponse(result)
 
 @permission_classes([AllowAny])
 class AccountView(APIView):
 
-    def get(self, request, *args, **kwargs) -> HttpResponse:
-        data = request.data['body']
-        _currency = data['_currency']
+    @swagger_auto_schema(   
+        operation_summary="계좌 정보 조회 하기",
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'api_key' : openapi.Schema(type=openapi.TYPE_STRING, description='api key'),
+                'api_secret' : openapi.Schema(type=openapi.TYPE_STRING, description='api secret'),
+                'currency' : openapi.Schema(type=openapi.TYPE_STRING, description='통화 종류')
+            }
+        ),
+        tags=["bithumb"],
+        operation_description="retrieve order list",
+    )  
+    def post(self, request, *args, **kwargs) -> HttpResponse:
+        data = request.data
+        _currency = data['currency']
         _api_key = data['api_key']
         _api_secret = data['api_secret']    
 
@@ -115,18 +159,35 @@ class AccountView(APIView):
             result = bithumb.get_balance(
                 currency=_currency
             )
+            #print(result)
 
         except Exception as e:
             return HttpResponse(e)
 
-        return HttpResponse(result)
+        return JsonResponse(result)
 
 
 @permission_classes([AllowAny])
 class OrderListView(APIView):
 
+    @swagger_auto_schema(   
+        operation_summary="주문 내역 조회 하기",
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'api_key' : openapi.Schema(type=openapi.TYPE_STRING, description='api key'),
+                'api_secret' : openapi.Schema(type=openapi.TYPE_STRING, description='api secret'),
+                'order_currency' : openapi.Schema(type=openapi.TYPE_STRING, description='통화 종류'),
+                'order_id' : openapi.Schema(type=openapi.TYPE_STRING, description='주문 uuid, 공백일 시 전체리스트 조회'),
+                'payment_currency' : openapi.Schema(type=openapi.TYPE_STRING, description='구매할 통화 KRW'),
+                'type' : openapi.Schema(type=openapi.TYPE_STRING, description='주문 종류')
+            }
+        ),
+        tags=["bithumb"],
+        operation_description="retrieve order list",
+    )  
     def post(self, request, *args, **kwargs) -> HttpResponse:
-        data = request.data['body']
+        data = request.data
         _api_key = data['api_key']
         _api_secret = data['api_secret']   
         _order_currency = data['order_currency']
@@ -157,8 +218,24 @@ class OrderListView(APIView):
 @permission_classes([AllowAny])
 class UnfinishedOrderListView(APIView):
 
-    def get(self, request, *args, **kwargs) -> HttpResponse:
-        data = request.data['body']
+    @swagger_auto_schema(   
+        operation_summary="미체결 주문 내역 조회 하기",
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'api_key' : openapi.Schema(type=openapi.TYPE_STRING, description='api key'),
+                'api_secret' : openapi.Schema(type=openapi.TYPE_STRING, description='api secret'),
+                'order_currency' : openapi.Schema(type=openapi.TYPE_STRING, description='주문할 통화 종류'),
+                'order_id' : openapi.Schema(type=openapi.TYPE_STRING, description='주문 uuid, 공백일 시 전체리스트 조회'),
+                'payment_currency' : openapi.Schema(type=openapi.TYPE_STRING, description='구매할 통화 KRW'),
+                'type' : openapi.Schema(type=openapi.TYPE_STRING, description='주문 종류')
+            }
+        ),
+        tags=["bithumb"],
+        operation_description="retrieve order list",
+    )  
+    def post(self, request, *args, **kwargs) -> HttpResponse:
+        data = request.data
         _api_key = data['api_key']
         _api_secret = data['api_secret']   
         _order_currency = data['order_currency']
@@ -188,8 +265,24 @@ class UnfinishedOrderListView(APIView):
 @permission_classes([AllowAny])
 class OrderCancelView(APIView):
 
+    @swagger_auto_schema(   
+        operation_summary="주문 취소 하기",
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'api_key' : openapi.Schema(type=openapi.TYPE_STRING, description='api key'),
+                'api_secret' : openapi.Schema(type=openapi.TYPE_STRING, description='api secret'),
+                'order_currency' : openapi.Schema(type=openapi.TYPE_STRING, description='주문할 통화 종류'),
+                'order_id' : openapi.Schema(type=openapi.TYPE_STRING, description='주문 uuid, 공백일 시 전체리스트 조회'),
+                'payment_currency' : openapi.Schema(type=openapi.TYPE_STRING, description='구매할 통화 KRW'),
+                'type' : openapi.Schema(type=openapi.TYPE_STRING, description='주문 종류')
+            }
+        ),
+        tags=["bithumb"],
+        operation_description="retrieve order list",
+    )  
     def post(self, request, *args, **kwargs) -> HttpResponse:
-        data = request.data['body']
+        data = request.data
         _api_key = data['api_key']
         _api_secret = data['api_secret']   
         _order_currency = data['order_currency']
